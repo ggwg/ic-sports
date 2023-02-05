dbc10ffd4803843d4b6d65c6edcf0d1c87094d3d
Gavin Wu <60797011+ggwg@users.noreply.github.com>
2023-02-05 11:36:18 +0000

    A

--- peter/main.py
+++ peter/main.py
@@ -1,261 +1,81 @@
-import network
-import video
-import traceback
-
-import asyncio
-import cv2
 import sys
-from contextlib import suppress, contextmanager
-from typing import Sequence
-from dataclasses import dataclass
-from ball_motion import Ball, draw, hand_meets_ball
-import random
-import time
-import pickle
+import game_ui
+from PyQt5.QtWidgets import (QApplication, QMainWindow, QLabel, QPushButton, 
+                             QVBoxLayout, QHBoxLayout, QWidget)
+from PyQt5.QtGui import (QFont, QPixmap)
+from PyQt5.QtCore import Qt
 
-import mediapipe as mp
-mp_pose = mp.solutions.pose
-mp_hands = mp.solutions.hands
+class MainMenu(QMainWindow):
+    def __init__(self):
+        super().__init__()
 
+        self.initUI()
+        
+    def initUI(self):
+        self.setWindowTitle('Main Menu')
 
-@contextmanager
-def dummy_resource():
-    try:
-        yield None
-    finally:
-        pass
+        # Create title
+        title = QLabel('My Virtual Space', self)
+        title.setAlignment(Qt.AlignCenter)
+        font = QFont()
+        font.setPointSize(24)
+        title.setFont(font)
 
+        # Create image
+        # loading image
+        image = QLabel(self)
+        pixmap = QPixmap('assets/beach_ball.png').scaledToWidth(200)
+        # adding image to label
+        image.setPixmap(pixmap)
+        image.resize(100, 100)
 
-@dataclass
-class Hand:
-    x: int
-    y: int
-    hit: bool
+        # Create description
+        description = QLabel('This is a game about adventure and discovery. Click the start button to begin.', self)
+        description.setAlignment(Qt.AlignCenter)
+        # description.setWordWrap(True)
+        description.setMaximumWidth(500)
 
-@dataclass
-class Score:
-    my: int = 0
-    competitor: int = 0
+        # Create start button
+        start_button = QPushButton('Start', self)
+        start_button.clicked.connect(self.start_clicked)
+        start_button.setFixedSize(100, 50)
+        start_button.setStyleSheet("background-color: green; color: white; font-size: 20px;")
 
+        # Set button layout
+        layout = QVBoxLayout()
+        layout.addSpacing(50)
+        layout.addWidget(title, alignment=Qt.AlignCenter)
+        layout.addSpacing(50)
+        layout.addWidget(image, alignment=Qt.AlignCenter)
+        layout.addSpacing(50)
+        layout.addWidget(description, alignment=Qt.AlignCenter)
+        layout.addSpacing(50)
+        button_layout = QHBoxLayout()
+        button_layout.addSpacing(450)
+        button_layout.addWidget(start_button)
+        layout.addLayout(button_layout)
+        central_widget = QWidget()
+        central_widget.setLayout(layout)
+        self.setCentralWidget(central_widget)
 
-async def h264_decode_worker(remote: network.Remote, decoder: video.VideoDec):
-    while True:
-        try:
-            frame = await remote.get_h264_frame()
-            decoder.write_h264(frame)
-        except Exception as e:
-            print("Error in decoding worker:", e)
+    def start_clicked(self):
+        # Link to another page when start button is clicked
+        self.next_page = game_ui.App()
+        self.next_page.show()
+        
+# class AnotherPage(QMainWindow):
+#     def __init__(self):
+#         super().__init__()
 
+#         self.initUI()
+        
+#     def initUI(self):
+#         self.setWindowTitle('Another Page')
 
-async def video_transmitter_worker(remote: network.Remote, encoder: video.VideoEnc):
-    while True:
-        try:
-            frame = await encoder.read_h264_async()
-            # print("got h264 frame")
-            await remote.send_h264_frame(frame)
-        except Exception as e:
-            print("Error in transmitter worker:", e)
-
-
-async def local_position_worker(queue: asyncio.Queue, player: Hand, ball: Ball, remote: network.Remote, is_hand=False):
-    i = 0
-    x_history = [-1, -1, -1]
-    y_history = [-1, -1, -1]
-    if is_hand:
-        with mp_hands.Hands(model_complexity=1, min_detection_confidence=0.5, min_tracking_confidence=0.5) as hands:
-            while True:
-                frame = await queue.get()
-
-                print("frame get")
-                # frame = cv2.resize(frame, (320, 240))
-                if i % 2 == 0:
-                    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                    results = hands.process(frame)
-
-                    if results.multi_hand_landmarks:
-                        hand_landmarks = results.multi_hand_landmarks[-1]
-                        x_history.append(
-                                640 - int(hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * 700))
-                        y_history.append(
-                            50 + int(hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * 400))
-                            
-                        x_history.pop(0)
-                        y_history.pop(0)
-
-                        # Exponentially weighted moving average
-                        player.x = x_history[0] * 0.161 + \
-                            x_history[1] * 0.296 + x_history[2] * 0.544
-                        player.y = y_history[0] * 0.161 + \
-                            y_history[1] * 0.296 + y_history[2] * 0.544
-                        print(f"HAND position: {player.x:.2f}, {player.y:.2f}")
-                        if ball.hitable and hand_meets_ball(ball, player):
-                            print("Hand hits ball")
-                            player.hit = True
-                            asyncio.create_task(remote.send_control(
-                                [ball.x, ball.y, -ball.z, -ball.dz]))
-                            ball.hit_back()
-                        else:
-                            player.hit = False
-    else:
-        with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
-            while True:
-                frame = await queue.get()
-                frame = cv2.resize(frame, (160, 120))
-                if i % 2 == 0:
-                    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                    results = pose.process(frame)
-
-                    if results.pose_landmarks:
-                        x_history.append(
-                            640 - int(results.pose_landmarks.landmark[0].x * 700))
-                        y_history.append(
-                            50 + int(results.pose_landmarks.landmark[0].y * 400))
-
-                        x_history.pop(0)
-                        y_history.pop(0)
-
-                        # Exponentially weighted moving average
-                        player.x = x_history[0] * 0.161 + \
-                            x_history[1] * 0.296 + x_history[2] * 0.544
-                        player.y = y_history[0] * 0.161 + \
-                            y_history[1] * 0.296 + y_history[2] * 0.544
-
-                        print(f"HEAD position: {player.x:.2f}, {player.y:.2f}")
-                        if ball.hitable and hand_meets_ball(ball, player):
-                            print("Head hits ball")
-                            player.hit = True
-                            asyncio.create_task(remote.send_control(
-                                [ball.x, ball.y, -ball.z, -ball.dz]))
-                            ball.hit_back()
-                        else:
-                            player.hit = False
-                        # print('Average left hand pos: ' + str(hands_average[0]), str(hands_average[1]))
-                i += 1
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    menu = MainMenu()
+    menu.show()
+    sys.exit(app.exec_())
 
 
-async def h264_encode_worker(queue: asyncio.Queue, encoder: video.VideoEnc):
-    while True:
-        try:
-            frame = await queue.get()
-            encoder.write_raw(frame)
-        except Exception as e:
-            print("Error in encoding worker:", e)
-
-
-async def frame_queue_tee(capture: video.VideoCap, queues: Sequence[asyncio.Queue]):
-    while True:
-        try:
-            frame = await capture.read_raw_async()
-            for queue in queues:
-                with suppress(asyncio.queues.QueueFull):
-                    queue.put_nowait(frame)
-        except Exception as e:
-            print("Error in frame tee:", e)
-
-
-async def server_ctrl_loop(remote: network.Remote, ball: Ball):
-    while True:
-        try:
-            ball.x, ball.y, ball.z, ball.dz = await remote.recv_control()
-            ball.hit_back()
-        except Exception as e:
-            print("Error in server ctrl loop:", e)
-
-
-async def client_ctrl_loop(remote: network.Remote, ball: Ball, score: Score):
-    while True:
-        try:
-            ball.x, ball.y, ball.z, ball.dz, score.competitor, score.my = await remote.recv_control()
-        except Exception as e:
-            print("Error in client ctrl loop:", e)
-
-
-async def server_logic_loop(ball: Ball, remote: network.Remote, hand: Hand, score: Score):
-    while True:
-        try:
-            await asyncio.sleep(0.02)
-            if remote.connected:
-                auto_hit_back = ball.update_pos()
-                if auto_hit_back:
-                    if ball.z > 0:
-                        score.competitor += 1
-                    else:
-                        score.my += 1
-                asyncio.create_task(remote.send_control(
-                    [ball.x, ball.y, -ball.z, -ball.dz, score.my, score.competitor]))
-        except Exception as e:
-            traceback.print_exc()
-            print("Error in server logic loop:", e)
-
-
-async def remote_render_worker(ball: Ball, hand: Hand, decoder: video.VideoDec, score: Score):
-    while True:
-        try:
-            frame = await decoder.read_raw_async()
-            frame = cv2.flip(frame, 1)
-            frame = draw(ball, frame)
-            print("ball pos:", ball)
-
-            color = (0, 0, 255) if not hand.hit else (255, 0, 0)
-            cv2.putText(frame, f"{score.my}:{score.competitor}", (video.IMAGE_WIDTH, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
-            cv2.circle(frame, (int(hand.x), int(hand.y)), 10, color, -1)
-            cv2.imshow("main", frame)
-            cv2.waitKey(1)
-        except Exception as e:
-            print("Error in remote render worker:", e)
-
-
-async def main():
-    if sys.argv[1] == "client":
-        remote = network.Client(sys.argv[2])
-        client_or_server = "client"
-    else:
-        remote = network.Server()
-        client_or_server = "server"
-
-    encoder = video.VideoEnc(is_server=(client_or_server == "server"))
-
-    frame_queue = asyncio.Queue(1)
-    frame_queue_2 = asyncio.Queue(1)
-    hand = Hand(0, 0, False)
-    ball = Ball(random.randint(100, 500), 300, 0)
-    score = Score()
-
-    with video.VideoDec() as decoder, video.VideoCap() as capture, encoder if client_or_server == "client" else dummy_resource():
-        def conn_on_cb(_):
-            nonlocal ball
-            print("Connection on")
-            ball = Ball(random.randint(100, 500), 300, 0)
-
-            encoder.run()
-
-        def conn_off_cb(_):
-            print("Connection off")
-            encoder.stop()
-            print("Encoder stopped")
-
-        remote.conn_on_cb = conn_on_cb
-        remote.conn_off_cb = conn_off_cb
-
-        async with remote:
-            print("starting")
-            asyncio.create_task(h264_decode_worker(remote, decoder))
-            asyncio.create_task(video_transmitter_worker(remote, encoder))
-            asyncio.create_task(h264_encode_worker(frame_queue, encoder))
-            asyncio.create_task(frame_queue_tee(
-                capture, (frame_queue, frame_queue_2)))
-            asyncio.create_task(local_position_worker(
-                frame_queue_2, hand, ball, remote, is_hand=True))
-            if client_or_server == "server":
-                asyncio.create_task(server_ctrl_loop(remote, ball))
-                asyncio.create_task(server_logic_loop(ball, remote, hand, score))
-            else:
-                asyncio.create_task(client_ctrl_loop(remote, ball, score))
-
-            asyncio.create_task(remote_render_worker(ball, hand, decoder, score))
-
-            await asyncio.Future()
-
-
-if __name__ == "__main__":
-    asyncio.run(main())
