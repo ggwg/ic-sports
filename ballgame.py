7a5da0e66386bd5424fbcf84042a5f7f2e3ebd3d
Gavin Wu <60797011+ggwg@users.noreply.github.com>
2023-02-05 03:33:18 +0000

    Linear interpolation for ball smoothing

--- ballgame.py
+++ ballgame.py
@@ -7,7 +7,7 @@ from math import (sqrt)
 import os
 import random
 
-SAMPLE_PER_FRAME = 6
+SAMPLE_PER_FRAME = 8
 
 class Ball:
     def __init__(self, x, y, z):
@@ -83,44 +83,112 @@ background = cv2.imread(filename)
 background = cv2.resize(background, (500, 500))
 
 # naturally fall down
-def loop(mp_drawing, mp_drawing_styles, mp_pose, cap):
-    hand_x_history = [-1, -1, -1]
-    hand_y_history = [-1, -1, -1]
-    sample_pos = 0
-
+def loop(pose, mp_pose, cap):
+    hand_x_history = [-1, -1, -1, -1, -1]
+    hand_y_history = [-1, -1, -1, -1, -1]
     ball = Ball(100, 400, 10)
     hand = Ball(100, 400, 10)
     ball.dz = 0.1
+
+    sample = 0
     while True:
+        sample += 1
+        if sample == SAMPLE_PER_FRAME // 4:
+            hand.x = statistics.fmean([
+                hand_x_history[0],
+                hand_x_history[1],
+                hand_x_history[2],
+                hand_x_history[3],
+                hand_x_history[4],
+            ])
+            hand.y = statistics.fmean([
+                hand_y_history[0],
+                hand_y_history[1],
+                hand_y_history[2],
+                hand_y_history[3],
+                hand_y_history[4],
+            ])
+        elif sample == SAMPLE_PER_FRAME // 2:
+            hand.x = statistics.fmean([
+                hand_x_history[0],
+                hand_x_history[1],
+                hand_x_history[2],
+                hand_x_history[3],
+                hand_x_history[4],
+                hand_x_history[4],
+            ])
+            hand.y = statistics.fmean([
+                hand_y_history[0],
+                hand_y_history[1],
+                hand_y_history[2],
+                hand_y_history[3],
+                hand_y_history[4],
+                hand_y_history[4],
+            ])
+        elif sample == 3*(SAMPLE_PER_FRAME // 4):
+            hand.x = statistics.fmean([
+                hand_x_history[0],
+                hand_x_history[1],
+                hand_x_history[2],
+                hand_x_history[3],
+                hand_x_history[4],
+                hand_x_history[4],
+                hand_x_history[4],
+            ])
+            hand.y = statistics.fmean([
+                hand_y_history[0],
+                hand_y_history[1],
+                hand_y_history[2],
+                hand_y_history[3],
+                hand_y_history[4],
+                hand_y_history[4],
+                hand_y_history[4],
+            ])
+        if sample == SAMPLE_PER_FRAME:
+            hand.x = statistics.fmean([
+                hand_x_history[0],
+                hand_x_history[1],
+                hand_x_history[2],
+                hand_x_history[3],
+                hand_x_history[4],
+                hand_x_history[4],
+                hand_x_history[4],
+                hand_x_history[4],
+            ])
+            hand.y = statistics.fmean([
+                hand_y_history[0],
+                hand_y_history[1],
+                hand_y_history[2],
+                hand_y_history[3],
+                hand_y_history[4],
+                hand_y_history[4],
+                hand_y_history[4],
+                hand_y_history[4],
+            ])
+            sample = 0
+            if cap.isOpened():
+                success, image = cap.read()
+                if not success:
+                    print("Ignoring empty camera frame.")
+                else:
+                    results = pose.process(image)
 
-        sample_pos += 1 
-        if sample_pos == SAMPLE_PER_FRAME:
-            with mp_pose.Pose(
-                min_detection_confidence=0.5,
-                min_tracking_confidence=0.5) as pose:
-                if cap.isOpened():
-                    success, image = cap.read()
-                    if not success:
-                        print("Ignoring empty camera frame.")
-                    else:
-                        results = pose.process(image)
+                    if results.pose_landmarks:
+                        hand_x_history.append(500 - int(results.pose_landmarks.landmark[0].x * 400))
+                        hand_y_history.append(320 + int(results.pose_landmarks.landmark[0].y * 400))
+                        
+                        hand_x_history.pop(0)
+                        hand_y_history.pop(0)
+                        
+                        # Exponentially weighted moving average
+                        # hand.x = hand_x_history[0] * 0.161 + hand_x_history[1] * 0.296 + hand_x_history[2] * 0.544
+                        # hand.y = hand_y_history[0] * 0.161 + hand_y_history[1] * 0.296 + hand_y_history[2] * 0.544
+                        
+                        
+                        
+                        print(hand.x, hand.y)
+                        # print('Average left hand pos: ' + str(hands_average[0]), str(hands_average[1]))
 
-                        if results.pose_landmarks:
-                            hand_x_history.append(500 - int(results.pose_landmarks.landmark[0].x * 400))
-                            hand_y_history.append(320 + int(results.pose_landmarks.landmark[0].y * 400))
-                            
-                            hand_x_history.pop(0)
-                            hand_y_history.pop(0)
-                            
-                            # Exponentially weighted moving average
-                            hand.x = hand_x_history[0] * 0.161 + hand_x_history[1] * 0.296 + hand_x_history[2] * 0.544
-                            hand.y = hand_y_history[0] * 0.161 + hand_y_history[1] * 0.296 + hand_y_history[2] * 0.544
-                            
-                            
-                            
-                            print(hand.x, hand.y)
-                            # print('Average left hand pos: ' + str(hands_average[0]), str(hands_average[1]))
-            sample_pos = 0
 
         
 
@@ -158,4 +226,7 @@ mp_drawing_styles = mp.solutions.drawing_styles
 mp_pose = mp.solutions.pose
 
 cap = cv2.VideoCapture(0)
-loop(mp_drawing, mp_drawing_styles, mp_pose, cap)
+with mp_pose.Pose(
+                min_detection_confidence=0.5,
+                min_tracking_confidence=0.5) as pose:
+    loop(pose, mp_pose, cap)
